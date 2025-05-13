15c41cec1356101d28433ffd4c3fb287dcf90e6e
Suman Bhatta <sumanbhatta735@gmail.com>
2025-05-13 08:05:11 +0545

    Implement user registration view with welcome email task

--- user_service/users/views.py
+++ user_service/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .tasks import send_welcome_email
 
 # Create your views here.
+
+def register_user(request):
+    username = request.GET.get('username', 'Guest')
+    send_welcome_email.delay(username)
+    return JsonResponse({"message": f"User {username} registered!"})
